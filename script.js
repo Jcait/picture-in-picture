f47d3c7e7f0db5de0a275d8145924c9975d410bb
Jcait <27211740+Jcait@users.noreply.github.com>
2022-03-29 22:53:37 +0100

    Picture in Picture page

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("hello")
+const videoElement = document.getElementById('video')
+const button = document.getElementById('button')
+
+// Async prompt to select media stream, pass to video element then play
+
+async function selectMediaStream() {
+    try { 
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch(error) {
+        console.log("SlectMediaStream broke", error)
+    }
+}
+
+button.addEventListener('click', async () => {
+    // disable button when clicked
+    button.disabled = true;
+    // start picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+});
+
+selectMediaStream()
